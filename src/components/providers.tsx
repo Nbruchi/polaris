3dbfdf72be63ff6088934022b799292066af3413
Nbruchi <nkundabagenzibruce@gmail.com>
2026-02-25 13:03:37 +0200

    Database setup

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,20 +1,45 @@
+"use client";
+
 import { ReactNode } from "react";
 import { ThemeProvider } from "./theme-provider";
 import { TooltipProvider } from "./ui/tooltip";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import {
+    Authenticated,
+    AuthLoading,
+    ConvexReactClient,
+    Unauthenticated,
+} from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import UnAuthenticatedView from "@/features/auth/components/unauthenticated-view";
+import { AuthLoadingView } from "@/features/auth/components/auth-loading-view";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const Providers = ({ children }: { children: ReactNode }) => {
     return (
         <ClerkProvider appearance={{ theme: dark }}>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                disableTransitionOnChange
-            >
-                <TooltipProvider>{children}</TooltipProvider>
-            </ThemeProvider>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <TooltipProvider>
+                        <Authenticated>
+                            {children}
+                        </Authenticated>
+                        <Unauthenticated>
+                            <UnAuthenticatedView/>
+                        </Unauthenticated>
+                        <AuthLoading>
+                            <AuthLoadingView/>
+                        </AuthLoading>
+                    </TooltipProvider>
+                </ThemeProvider>
+            </ConvexProviderWithClerk>
         </ClerkProvider>
     );
 };
